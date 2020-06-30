19611e3b6801399780dccd076aed0430040d6486
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-06-30 21:45:52 +0200

    Add attribute rendering to HTML5StringRenderer

--- src/Rendering/HTML5StringRenderer.php
+++ src/Rendering/HTML5StringRenderer.php
@@ -1,8 +1,11 @@
 <?php declare(strict_types=1);
 namespace PackageFactory\VirtualDOM\Rendering;
 
+use PackageFactory\VirtualDOM\Attribute;
+use PackageFactory\VirtualDOM\Attributes;
 use PackageFactory\VirtualDOM\DangerouslyUnescapedText;
 use PackageFactory\VirtualDOM\Element;
+use PackageFactory\VirtualDOM\Escaper;
 use PackageFactory\VirtualDOM\Fragment;
 use PackageFactory\VirtualDOM\Node;
 use PackageFactory\VirtualDOM\NodeList;
@@ -22,18 +25,16 @@ final class HTML5StringRenderer
                     return sprintf(
                         '<%s%s/>',
                         $node->getElementType()->getTagName(),
-                        $node->getAttributes()
+                        self::renderAttributes($node->getAttributes())
                     );
-                }
-                else {
+                } else {
                     return sprintf(
                         '<%1$s%2$s></%1$s>',
                         $node->getElementType()->getTagName(),
-                        $node->getAttributes()
+                        self::renderAttributes($node->getAttributes())
                     );
                 }
-            }
-            else {
+            } else {
                 return sprintf(
                     '<%1$s%2$s>%3$s</%1$s>',
                     $node->getElementType()->getTagName(),
@@ -49,6 +50,42 @@ final class HTML5StringRenderer
             return $node->getValue();
         } else {
             throw RenderingFailed::becauseOfAnUnknownNodeClass($node);
+        }
+    }
+
+    /**
+     * @param Attributes $attributes
+     * @return string
+     */
+    public static function renderAttributes(Attributes $attributes): string
+    {
+        if ($attributes->count() === 0) {
+            return '';
+        } else {
+            $result = '';
+
+            foreach ($attributes as $attribute) {
+                $result .= self::renderAttribute($attribute);
+            }
+    
+            return $result;
+        }
+    }
+
+    /**
+     * @param Attribute $attribute
+     * @return string
+     */
+    public static function renderAttribute(Attribute $attribute): string
+    {
+        if ($attribute->getIsBoolean()) {
+            return ' ' . $attribute->getName();
+        } else {
+            return sprintf(
+                ' %s="%s"',
+                $attribute->getName(),
+                Escaper::escapeAttributeValue($attribute->getValue())
+            );
         }
     }
 
